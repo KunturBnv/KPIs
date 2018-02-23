5126c6f5437556852494f34a2d0c70127beb8681
KainaK0 <johancb1411@gmail.com>
2018-02-23 13:41:53 -0500

    da and do, witout empty values

--- Prueba01.R
+++ Prueba01.R
@@ -2,3 +2,9 @@ setwd("~/Repository")
 
 da <-read.csv("Avisos 022018 IW29.CSV")
 do <-read.csv("Ordenes 022018 IW39.CSV")
+
+da <- da[,colSums(is.na(da))<nrow(da)]
+da[da == ""]<-NA
+do <- do[,colSums(is.na(do))<nrow(do)]
+do[do == ""]<-NA
+tot <- merge(do,da,by = "Orden",all = FALSE)

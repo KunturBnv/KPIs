a1f003a80a54b769e51c76fff3a9f21d69053daa
KainaK0 <johancb1411@gmail.com>
2018-02-25 16:45:52 -0500

    comment 2 lines useless

--- Prueba01.R
+++ Prueba01.R
@@ -9,5 +9,5 @@ do <- do[,colSums(is.na(do))<nrow(do)]
 do[do == ""]<-NA
 tot <- merge(do,da,by = "Orden",all = FALSE)
 
-tot1[,"Fin.de.avería"] = paste(tot1$Fin.de.avería[!is.na(tot1$Fin.de.avería)],tot1$Hora.fin.avería,sep =" ")
-tot1[,"Hora.fin.avería"] = as.POSIXct(tot1$Hora.fin.avería, format="%H:%M:%S")
+#tot1[,"Fin.de.avería"] = paste(tot1$Fin.de.avería[!is.na(tot1$Fin.de.avería)],tot1$Hora.fin.avería,sep =" ")
+#tot1[,"Hora.fin.avería"] = as.POSIXct(tot1$Hora.fin.avería, format="%H:%M:%S")

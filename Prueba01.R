2974383997632ce24f1ca5adbb70eee46a2bc95d
KainaK0 <johancb1411@gmail.com>
2018-03-01 11:54:41 -0500

    Adding Excel getway of data

--- Prueba01.R
+++ Prueba01.R
@@ -1,13 +1,19 @@
-setwd("~/Repository")
+setwd("~/KPIs")
+library(readxl)
 
-da <-read.csv("Avisos 022018 IW29.CSV")
-do <-read.csv("Ordenes 022018 IW39.CSV")
+#da <-read.csv("Avisos 022018 IW29.CSV")
+#do <-read.csv("Ordenes 022018 IW39.CSV")
 
-da <- da[,colSums(is.na(da))<nrow(da)]
+dae <- read_excel("Avisos 030118 IW29.xlsx")
+
+dae <- da[,colSums(is.na(da))<nrow(da)]
 da[da == ""]<-NA
-do <- do[,colSums(is.na(do))<nrow(do)]
-do[do == ""]<-NA
-tot <- merge(do,da,by = "Orden",all = FALSE)
+
+# do <- do[,colSums(is.na(do))<nrow(do)]
+# do[do == ""]<-NA
+
+#tot <- merge(do,da,by = "Orden",all = FALSE)
+
 
 #tot1[,"Fin.de.avería"] = paste(tot1$Fin.de.avería[!is.na(tot1$Fin.de.avería)],tot1$Hora.fin.avería,sep =" ")
 #tot1[,"Hora.fin.avería"] = as.POSIXct(tot1$Hora.fin.avería, format="%H:%M:%S")
